a7852e3055b1a607a65d30af1a0bbafdb79d12be
derrickallenlo <dalo@uci.edu>
2014-03-05 23:38:18 -0800

    Init Structure of invertedPageTable and TLB Miss Handler
    Still need to determine how to add pid/vpn’s to page table. Need to
    create a TLB manager to enforce the replacement policy. Probably LRU.

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -1,5 +1,6 @@
 package nachos.vm;
 
+import java.util.Hashtable;
 import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
@@ -8,44 +9,62 @@ import nachos.vm.*;
 /**
  * A kernel that can support multiple demand-paging user processes.
  */
-public class VMKernel extends UserKernel {
+public class VMKernel extends UserKernel 
+{
+	// dummy variables to make javac smarter
+	private static VMProcess dummy1 = null;
+	private static final char dbgVM = 'v';
+        public static Hashtable invertedPageTable;
+        public static Lock invertedPageTableLock;
+        
 	/**
 	 * Allocate a new VM kernel.
 	 */
-	public VMKernel() {
+	public VMKernel() 
+        {
 		super();
 	}
 
 	/**
 	 * Initialize this kernel.
 	 */
-	public void initialize(String[] args) {
-		super.initialize(args);
+	public void initialize(String[] args) 
+        {
+		super.initialize(args); 
+                initializeInvertedPageTable();
+                
 	}
 
 	/**
 	 * Test this kernel.
 	 */
-	public void selfTest() {
+	public void selfTest() 
+        {
 		super.selfTest();
 	}
 
 	/**
 	 * Start running user programs.
 	 */
-	public void run() {
+	public void run() 
+        {
 		super.run();
 	}
 
 	/**
 	 * Terminate this kernel. Never returns.
 	 */
-	public void terminate() {
+	public void terminate() 
+        {
 		super.terminate();
 	}
-
-	// dummy variables to make javac smarter
-	private static VMProcess dummy1 = null;
-
-	private static final char dbgVM = 'v';
+        
+        private void initializeInvertedPageTable()
+        {
+            invertedPageTable = new Hashtable();
+            invertedPageTableLock = new Lock();
+            
+        }
+        
+        //TLB Manager? To manage replacement policy
 }
--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -8,11 +8,16 @@ import nachos.vm.*;
 /**
  * A <tt>UserProcess</tt> that supports demand-paging.
  */
-public class VMProcess extends UserProcess {
+public class VMProcess extends UserProcess 
+{
+	private static final int  pageSize = Processor.pageSize;
+	private static final char dbgProcess = 'a';
+	private static final char dbgVM = 'v';
 	/**
 	 * Allocate a new process.
 	 */
-	public VMProcess() {
+	public VMProcess() 
+        {
 		super();
 	}
 
@@ -20,7 +25,8 @@ public class VMProcess extends UserProcess {
 	 * Save the state of this process in preparation for a context switch.
 	 * Called by <tt>UThread.saveState()</tt>.
 	 */
-	public void saveState() {
+	public void saveState() 
+        {
 		super.saveState();
 	}
 
@@ -28,7 +34,8 @@ public class VMProcess extends UserProcess {
 	 * Restore the state of this process after a context switch. Called by
 	 * <tt>UThread.restoreState()</tt>.
 	 */
-	public void restoreState() {
+	public void restoreState() 
+        {
 		super.restoreState();
 	}
 
@@ -38,14 +45,16 @@ public class VMProcess extends UserProcess {
 	 * 
 	 * @return <tt>true</tt> if successful.
 	 */
-	protected boolean loadSections() {
+	protected boolean loadSections() 
+        {
 		return super.loadSections();
 	}
 
 	/**
 	 * Release any resources allocated by <tt>loadSections()</tt>.
 	 */
-	protected void unloadSections() {
+	protected void unloadSections() 
+        {
 		super.unloadSections();
 	}
 
@@ -56,19 +65,47 @@ public class VMProcess extends UserProcess {
 	 * 
 	 * @param cause the user exception that occurred.
 	 */
-	public void handleException(int cause) {
+	public void handleException(int cause) 
+        {
 		Processor processor = Machine.processor();
 
-		switch (cause) {
-		default:
+		switch (cause) 
+                {
+                    case Processor.exceptionTLBMiss:
+                        handleTLBMiss();
+                        break;
+                    default:
 			super.handleException(cause);
 			break;
 		}
 	}
-
-	private static final int pageSize = Processor.pageSize;
-
-	private static final char dbgProcess = 'a';
-
-	private static final char dbgVM = 'v';
+        
+        /**
+	 * Handle a TLB Miss exception.
+	 * Page translation was not found in TLB, translate Page # from inverted Page Table
+	 * 
+	 */
+        private void handleTLBMiss()
+        {
+            int missedVirtualAddress = Machine.processor().readRegister(Processor.regBadVAddr);
+            int virtualPageNumber = 0;
+            
+            System.out.println("TLB Miss! On Virtual Address " + missedVirtualAddress);
+            
+            //Retrieve Page # translation and resume process?
+            
+            //acquire lock to invertedPageTable
+            VMKernel.invertedPageTableLock.acquire();
+            
+            //Pass in Key (PID) to the Hash table to return the Virtual Page #
+            virtualPageNumber = (int) VMKernel.invertedPageTable.get(super.getProcessID());
+            System.out.println("Associated PID: " + " & Virtual Page #" +  virtualPageNumber);
+            
+            //Combine VPN with Virtual Address to determine Physical Address
+            
+            
+            
+            //Write out translation to TLB
+            
+        }
 }
