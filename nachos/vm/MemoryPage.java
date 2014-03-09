f86d9228d0932d8a6cbe7fd7fc3268c7e5c464d0
derrickallenlo <dalo@uci.edu>
2014-03-08 18:22:41 -0800

    Base Implementation of Part I
    Still need page fault handling code before anything is functional.

---
+++ nachos/vm/MemoryPage.java
@@ -0,0 +1,21 @@
+package nachos.vm;
+
+import nachos.machine.TranslationEntry;
+
+/**
+ *
+ * @author derrick
+ */
+public class MemoryPage 
+{
+    int processId;
+    int virtualPageNumber;
+    TranslationEntry entry;
+
+    public MemoryPage(int processId,int virtualPageNumber, TranslationEntry entry) 
+    {
+            this.processId = processId;
+            this.virtualPageNumber = virtualPageNumber;
+            this.entry = entry;
+    }
+}
---
+++ nachos/vm/TLBController.java
@@ -0,0 +1,59 @@
+package nachos.vm;
+
+import nachos.machine.Lib;
+import nachos.machine.Machine;
+import nachos.machine.TranslationEntry;
+
+/**
+ *
+ * @author derrick
+ */
+public class TLBController 
+{
+    public void addEntry(TranslationEntry entry)
+    {
+        //Search for invalidEntries and replace first one that is found
+        for (int i = 0; i < Machine.processor().getTLBSize(); i++)
+        {
+            if (!Machine.processor().readTLBEntry(i).valid) 
+            {
+                Machine.processor().writeTLBEntry(i, entry);
+                return;
+            }
+        }
+             
+        //If table is full..
+        Machine.processor().writeTLBEntry(Lib.random(Machine.processor().getTLBSize()), entry);
+        
+        //Search for oldest and replace
+        //TODO
+    }
+    public void flush()
+    {
+        //Write all TLB entries back to the Page Table
+        for (int i = 0; i < Machine.processor().getTLBSize(); i++)
+        {
+            TranslationEntry currentEntry = Machine.processor().readTLBEntry(i);
+            
+            if (currentEntry.valid)
+            {
+                VMKernel.physicalMemoryMap[currentEntry.ppn].entry = currentEntry;
+            }
+        }
+    }
+    
+    //clear all TLB entries by invalidating all entries
+    public void clear()
+    {
+        for (int i = 0; i < Machine.processor().getTLBSize(); i++) 
+        {
+                TranslationEntry entry = Machine.processor().readTLBEntry(i);
+		if (entry.valid)
+                {
+			VMKernel.physicalMemoryMap[entry.ppn].entry = entry;
+                        entry.valid = false;
+                        Machine.processor().writeTLBEntry(i, entry);
+                }
+        }
+    }
+}
